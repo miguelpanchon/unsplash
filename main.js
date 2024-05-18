a1a457aab2c2c4fedbd77fd2c74bf251b9b1aa4f
miguelpanchon <mp607586086@gmail.com>
2024-05-18 22:36:22 +0200

    update js. it is working now

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { paintFooter } from "./src/components/Footer/Footer";
+import { paintHeader } from "./src/components/Header/Header"
+// import { PhotoPaintRandom } from "./src/components/MainPhotos/PhotoPaintRandom";
+// import { PhotoSearchRandom } from "./src/components/PhotoSearchRandom/PhotoSearchRandom";
+import "./style.css";
+import { paintLogo2 } from "./src/components/Header/Logo/Logo2";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+paintFooter();
+paintHeader();
+PhotoPaintRandom();
 
-setupCounter(document.querySelector('#counter'))
+// PhotoPaintRandom();
