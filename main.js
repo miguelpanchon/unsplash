bf767794986a56128c45b79b37408746221f41e2
miguelpanchon <mp607586086@gmail.com>
2024-05-18 23:38:56 +0200

    upload new location of icons not seen when deployed...

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import { paintFooter } from "./src/components/Footer/Footer";
 import { paintHeader } from "./src/components/Header/Header"
-import { PhotoPaintRandom } from "./src/components/MainPhotos/PhotoPaintRandom";
 // import { PhotoSearchRandom } from "./src/components/PhotoSearchRandom/PhotoSearchRandom";
 import "./style.css";
 // import { paintLogo2 } from "./src/components/Header/Logo/Logo2";
