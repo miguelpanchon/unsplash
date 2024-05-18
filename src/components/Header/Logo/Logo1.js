45edd726fd1549a4547cda4aeda448385a7c3717
miguelpanchon <mp607586086@gmail.com>
2024-05-19 00:47:57 +0200

    update path of logo 1 and 2

--- src/components/Header/Logo/Logo1.js
+++ src/components/Header/Logo/Logo1.js
@@ -2,7 +2,7 @@ import './Logo1.css';
 export function paintLogo1() {
     console.log("entra en paintLogo1");
     const logoElement = document.createElement("img");
-    logoElement.src = "./public/unsplash.jpg";
+    logoElement.src = "./unsplash.jpg";
     logoElement.alt = "unsplash logo...";
     logoElement.className = "Logo1";
 
